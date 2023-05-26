13280fca5682725ebc0210783fef86c01f3917e1
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2023-05-26 22:34:10 +0200

    Added twitter bot to bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
+import tweepy
 
-
-def use_bots(config):
+def use_bots(config, message):
     
     # check if discord
     
-    
     # check if twitter
-    
-    
-    pass
+    if config.getboolean("TWITTER", "ENABLE"):
+        
+        auth = tweepy.OAuthHandler(config.get("TWITTER", "API_KEY"), config.get("TWITTER", "API_SECRET_KEY"))
+        auth.set_access_token(config.get("TWITTER", "ACCESS_TOKEN"), config.get("TWITTER", "ACCESS_TOKEN_SECRET"))
+
+        api = tweepy.API(auth)
+        
+        api.update_status(message)
--- monitor.py
+++ monitor.py
@@ -2,13 +2,11 @@ import time
 from yt_search import get_newest_video_id, get_transcript
 from openai_client import summarize_urban_rescue
 import configparser
+import bot
 
 main_config = configparser.ConfigParser()
 
 main_config.read("./configs/mainconf.ini")
-
-def do_stuff():
-    print("Bom! Done:D:D:D")
 
 if __name__ == "__main__":
     
@@ -20,7 +18,9 @@ if __name__ == "__main__":
             if currentID != newID:
                 currentID = newID
                 transcript = get_transcript(currentID)
-                do_stuff()
+                
+                bot.use_bots(main_config, summarize_urban_rescue(transcript, main_config))
+                
             time.sleep(1800) # Sleep for 1800 seconds (30 minutes)
         except Exception as e:
             print("An error occurred:", e)
